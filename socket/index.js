f416bea86901d3473b5cc8395f676d25407c50df
shafaqueimroz <imrozshafaque@gmail.com>
2024-10-22 02:08:54 +0530

    corsUpdatedAtSoxketIoAsWell

--- socket/index.js
+++ socket/index.js
@@ -11,7 +11,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["https://shz-chat-app.netlify.app", "http://localhost:5173"],
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
